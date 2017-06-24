dbe24a24b7f09766f45f167908663c590f937263
Alwin Solanky <alwinsolanky@gmail.com>
2017-06-24 12:44:14 +0100

    Added new classes to Engine

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    class HealingPotion
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string NamePlural { get; set; }
+
+        public int AmountToHeal { get; set; }
+    }
+}
